8e66c31527fd96db72e3262f3d85dcdc52669a2c
Timofey06 <loktionov.tv@phystech.edu>
2025-12-15 16:58:27 +0300

    some_fix

--- intern/booking/infrastructure/kafka/producer.go
+++ intern/booking/infrastructure/kafka/producer.go
@@ -10,7 +10,6 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-// AsyncProducer writes messages to Kafka in a fire-and-forget fashion.
 type AsyncProducer struct {
 	writer *kafka.Writer
 }
@@ -19,7 +18,7 @@ func NewProducer(brokers string, topic string) interfaces.Producer {
 	w := &kafka.Writer{
 		Addr:         kafka.TCP(brokers),
 		Topic:        topic,
-		Async:        false, // we'll write in separate goroutine ourselves
+		Async:        false,
 		RequiredAcks: kafka.RequireOne,
 		Balancer:     &kafka.LeastBytes{},
 		WriteTimeout: 5 * time.Second,
@@ -28,16 +27,13 @@ func NewProducer(brokers string, topic string) interfaces.Producer {
 }
 
 func (p *AsyncProducer) Send(message []byte) {
-	// Fire-and-forget: launch goroutine, don't block caller
 	go func(msg []byte) {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		_ = p.writer.WriteMessages(ctx, kafka.Message{Value: msg})
-		// errors intentionally ignored (fire-and-forget). If desired, this could log via a logger passed at creation.
 	}(message)
 }
 
-// convenience helper to send arbitrary structs as JSON
 func (p *AsyncProducer) SendStruct(v any) {
 	b, _ := json.Marshal(v)
 	p.Send(b)
